c852286cd2664d820db62a7284bc95954ae286bd
Mike Picus <misha9584@gmail.com>
2022-05-04 10:24:21 -0700

    adding json to index html

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for, request, abort, jsonify, make_response
+# import os
+import json
+# import html
 
 app = Flask(__name__)
 
+# read file
+with open('./database/recipe.json', 'r') as myfile:
+    data = myfile.read()
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html', title="page", jsonfile=json.dumps(data)) 
 
 @app.route('/recipe/create')
 def create():
     return render_template('recipe_create.html') 
+
+
 
 @app.route('/recipe/view/<recipe_id>')
 def recipe_view():
@@ -40,5 +47,16 @@ if __name__ == '__main__':
 #     email = req["email"]
 #     password = req["password"]
 
+#     return make_response(jsonify({email:email, password:password}), 200)
 
+# read file
+# with open('./database/recipe.json', 'r') as myfile:
+#     data = myfile.read()
+
+
+
+# @app.route("/")
+# def index():
+    
+#     return render_template('index.html', mike_test = test, title="page", jsonfile=json.dumps(data))
 
