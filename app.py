4d4c2e96d2a76118f13146ee38df356b437ba469
Tony Cheng <91754279+StrangerXDD@users.noreply.github.com>
2022-05-04 21:32:32 -0700

    Revert " Homepage render recipes from Json (to develop branch)"

--- app.py
+++ app.py
@@ -1,35 +1,28 @@
-# Ask Mike Picus if something is not clear in this file
+from flask import Flask, render_template
+
 
 app = Flask(__name__,template_folder='./spork/templates')
 
-# index/home page - renders info from recipe.json
+
 @app.route('/')
 def index():
-    with open('./spork/database/recipe.json', 'r') as myfile:
-        data = json.loads(myfile.read())
-        for i in data:
-            x = i['Ingredients']
-    return render_template('index.html', jsonfile = data, ingredients = x) 
+    return render_template('index.html') 
 
-# recipe create page
 @app.route('/recipe/create')
 def create():
-    return render_template('/recipe/recipe_create.html') 
+    return render_template('recipe_create.html') 
 
-# recipe view page
 @app.route('/recipe/view/<recipe_id>')
 def recipe_view():
-    return render_template('/recipe/view_recipe.html') 
+    return render_template('view_recipe.html') 
 
-# register page
 @app.route('/register')
 def register():
-    return render_template('/user/login_register.html') 
+    return render_template('register_login.html') 
 
-# login page
 @app.route('/login')
 def login():
-    return render_template('/user/login_register.html') 
+    return render_template('register_login.html') 
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
