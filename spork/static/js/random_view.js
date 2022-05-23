3d7df6731a5149f7cd0aecd31ab1b7f1a1431020
StrangerXDD <tcheng69@my.bcit.ca>
2022-05-23 00:27:51 -0700

    fix random bug

--- spork/static/js/random_view.js
+++ spork/static/js/random_view.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
                 type: 'GET',
             })
             .done(function(data) {
-                $('#random-card').empty().append("<img src=\"{{url_for('static',filename='images/'+" + data['image'] + ")}} \" width=\"100% \" height=\"225 \" class=\"card-img-top \" text=\"Thumbnail \"><div class=\"card-body \"><h4 class=\"my-0 font-weight-normal \">{{" + data['title'] + "}}</h4><p class=\"card-text \">{{" + data['description'] + "}}</p><div class=\"d-flex justify-content-between align-items-center \"><div class=\"btn-group \"><a href=\"/recipe/view/{{" + data['recipeID'] + "}} \"><button type=\"button \" class=\"btn btn-sm btn-outline-secondary \">View</button></a><button type=\"button \" class=\"btn btn-sm btn-outline-secondary \">Rate</button></div><small class=\"text-muted \">Serves: {{" + data['serving'] + "}}</small></div></div></div>")
+                $('#random-card').empty().append("<img src=\""+ data['image_link'] + "\" width=\"100% \" height=\"225 \" class=\"card-img-top \" text=\"Thumbnail \"><div class=\"card-body \"><h4 class=\"my-0 font-weight-normal \">" + data['recipe']['title'] + "</h4><p class=\"card-text \">" + data['recipe']['description'] + "</p><div class=\"d-flex justify-content-between align-items-center \"><div class=\"btn-group \"><a href=\"/recipe/view/" + data['recipe']['recipeID'] + " \"><button type=\"button \" class=\"btn btn-sm btn-outline-secondary \">View</button></a><button type=\"button \" class=\"btn btn-sm btn-outline-secondary \">Rate</button></div><small class=\"text-muted \">Serves: " + data['recipe']['serving'] + "</small></div></div></div>")
             });
 
 
@@ -17,6 +17,3 @@ $(document).ready(function() {
 
     })
 })
-
-// "<a href=\"/recipe/view/"+data['recipeID']+ "><div class=\"card mb-4 box-shadow\"><div class=\"card-header\"><h4 class=\"my-0 font-weight-normal\">" + data['title'] + "</h4></div><div class=\"card-body\"><img src=\"{{url_for('static', filename='images/'+ " + data['image'] +
-//  ")}}\" alt=\"image\" width=\"100\" class=\"img-thumbnail\"><ul class=\"list-unstyled mt-3 mb-4\"><li> # of servings " + data['serving'] + " </li></ul><span id=\"number-of-ingredients\"> </span></div></div></a>"
