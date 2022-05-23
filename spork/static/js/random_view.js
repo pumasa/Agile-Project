f20192e4e540c1ed6c5f8994f35efedd2e971e7e
StrangerXDD <tcheng69@my.bcit.ca>
2022-05-22 18:05:41 -0700

    fix view bug

--- spork/static/js/random_view.js
+++ spork/static/js/random_view.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
             type: 'GET',
         })
             .done(function (data) {
-                $('#random-card').empty().append("<a href=\"/recipe/view/" + data['recipe']['recipeID'] + ">\"<div class=\"card mb-4 box-shadow\"><div class=\"card-header\"><h4 class=\"my-0 font-weight-normal\">" + data['recipe']['title'] + "</h4></div><div class=\"card-body\"><img src=\"" + data['image_link'] +
+                $('#random-card').empty().append("<a href=\"/recipe/view/" + data['recipe']['recipeID'] + "\"><div class=\"card mb-4 box-shadow\"><div class=\"card-header\"><h4 class=\"my-0 font-weight-normal\">" + data['recipe']['title'] + "</h4></div><div class=\"card-body\"><img src=\"" + data['image_link'] +
                     "\" alt=\"image\" width=\"100\" class=\"img-thumbnail\"><ul class=\"list-unstyled mt-3 mb-4\"><li> # of servings " + data['recipe']['serving'] + " </li></ul><span id=\"number-of-ingredients\"> </span></div></div></a>")
             });
         e.preventDefault();
