43fd1fbbe565a3e25d17587e73812938b89321cd
Mike <misha9584@gmail.com>
2022-05-23 12:54:27 -0700

    Updated the way recipe view is called, updated buttons to the author

--- spork/static/js/random_view.js
+++ spork/static/js/random_view.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
                 type: 'GET',
             })
             .done(function(data) {
-                $('#random-card').empty().append("<img src=\"{{url_for('static',filename='images/'+" + data['image'] + ")}} \" width=\"100% \" height=\"225 \" class=\"card-img-top \" text=\"Thumbnail \"><div class=\"card-body \"><h4 class=\"my-0 font-weight-normal \">{{" + data['title'] + "}}</h4><p class=\"card-text \">{{" + data['description'] + "}}</p><div class=\"d-flex justify-content-between align-items-center \"><div class=\"btn-group \"><a href=\"/recipe/view/{{" + data['recipeID'] + "}} \"><button type=\"button \" class=\"btn btn-sm btn-outline-secondary \">View</button></a><button type=\"button \" class=\"btn btn-sm btn-outline-secondary \">Rate</button></div><small class=\"text-muted \">Serves: {{" + data['serving'] + "}}</small></div></div></div>")
+                $('#random-card').empty().append("<a href=\"/recipe/view/" + data['recipe']['recipeID'] + "\"><div class=\"col-md-10\" style=\"margin-left: auto;margin-right: auto;\"><img src=\"" + data['image_link'] + "\" width=\"100% \" height=\"225 \" class=\"card-img-top \" text=\"Thumbnail \"><div class=\"card-body \"><h4 class=\"my-0 font-weight-normal \">" + data['recipe']['title'] + "</h4><p class=\"card-text \">" + data['recipe']['description'] + "</p><div class=\"d-flex justify-content-between align-items-center \"><div class=\"btn-group \"><small class=\"text-muted \">By: " + data['recipe']['author'] + "</small> </div><small class=\"text-muted \">Serves: " + data['recipe']['serving'] + "</small></div></div></div></a></div>")
             });
 
 
